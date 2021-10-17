c08f3f03e4ab1eef6a6ecb8c7b79a6e5ffb64fb7
rahulpannu4455 <92663517+rahulpannu4455@users.noreply.github.com>
2021-10-17 19:26:11 +0530

    Add files via upload

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {useSelector} from 'react-redux'
+
+// import useStyles from "./styles";
+
+
+const Post = () =>{
+    const posts = useSelector((state)=>state.posts)
+    // const classes = useStyles();
+    console.log(posts);
+    return (
+        <h1>
+            Post
+        </h1>
+    )
+}
+
+export default Post;
